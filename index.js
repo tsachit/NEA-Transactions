701ba138d24a21a20134614abaede9278b0f4c26
Sachit Karki <sachitjungkarki@gmail.com>
2020-08-10 12:36:04 +0545

    get port from env

--- index.js
+++ index.js
@@ -72,6 +72,7 @@ app.post("/", function(req, res) {
   })();
 });
 
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
